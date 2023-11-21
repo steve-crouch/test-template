22b157fe0a5c21cb8177fa2e2924da3e85a08783
Steve Crouch <s.crouch@software.ac.uk>
2023-11-21 14:37:28 +0000

    Initialize issue-3-document-rainfall

---
+++ rainfall_conversion.py
@@ -0,0 +1,12 @@
+def inches_to_mm(inches):
+    """
+    Converts rainfall in inches to mm
+
+    Arguments:
+        inches: rainfall in inches
+
+    Returns: rainfall in mm
+    """
+    mm = inches * 25.4
+    return mm
+
